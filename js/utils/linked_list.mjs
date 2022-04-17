41cde9f73548bb054936c01e2c938d467358c5ce
FizzyElt <fizzyelt8786@gmail.com>
2022-04-17 22:18:07 +0800

    implement some linklist method

--- js/utils/linked_list.mjs
+++ js/utils/linked_list.mjs
@@ -2,3 +2,44 @@ export function ListNode(val, next) {
   this.val = val === undefined ? 0 : val;
   this.next = next === undefined ? null : next;
 }
+
+export function insertNode(head, val, index, current = 0) {
+  if (!head) {
+    return new ListNode(val, null);
+  }
+
+  if (index === current) {
+    const node = new ListNode(val, head);
+    return node;
+  }
+
+  head.next = insertNode(head.next, val, index, current + 1);
+
+  return head;
+}
+
+export function insertNodeToEnd(head, val) {
+  if (!head) {
+    return new ListNode(val, null);
+  }
+
+  head.next = insertNodeToEnd(head.next, val);
+
+  return head;
+}
+
+export function insertNodeToFront(node, val) {
+  return insertNode(node, val, 0);
+}
+
+export function transformListNodeToArray(node) {
+  let current = node;
+  let arr = [];
+
+  while (current !== null) {
+    arr.push(current.val);
+    current = current.next;
+  }
+
+  return arr;
+}
