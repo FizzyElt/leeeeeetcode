7c1d24d74a733c89997787c3d74e0b371f93a834
FizzyElt <fizzyelt8786@gmail.com>
2022-04-18 22:13:35 +0800

    138 js solution

---
+++ js/138_copy_list_with_random_pointer/index.mjs
@@ -0,0 +1,35 @@
+function copyRandomList(head) {
+  const originList = [];
+
+  let current = head;
+  while (current !== null) {
+    originList.push(current);
+    current = current.next;
+  }
+
+  const list = [];
+
+  current = head;
+
+  while (current !== null) {
+    const node = new Node(current.val, null, current.random);
+
+    list.push(node);
+    current = current.next;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    list[i].next = list[i + 1] ?? null;
+
+    const originRandom = list[i].random;
+    
+    if (originRandom !== null) {
+      const index = originList.findIndex((node) => node === originRandom);
+      list[i].random = list[index];
+    } else {
+      list[i].random = null;
+    }
+  }
+
+  return list[0] ?? null;
+}
