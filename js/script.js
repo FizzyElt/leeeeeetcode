8e8cceda505fa0ec7d652082d02ac28a0fef94e3
FizzyElt <fizzyelt8786@gmail.com>
2022-11-05 00:28:07 +0800

    add js readme

--- js/script.js
+++ js/script.js
@@ -6,13 +6,20 @@ fs.readdir('./', { withFileTypes: true }, (err, files) => {
     return;
   }
 
-  files.forEach((file) => {
-    if (file.isDirectory()) {
-      const oldPath = path.join('.', file.name);
-      const [id, ...rest] = file.name.split('-');
-      const newPath = path.join('.', `${id}_${rest.join('-')}`);
+  const questions = files
+    .filter((file) => {
+      return file.isDirectory();
+    })
+    .map((file) => file.name.split('_'))
+    .filter(([id, name]) => !isNaN(Number(id)) && !!name)
+    .sort((a, b) => {
+      const numA = parseInt(a[0]);
+      const numB = parseInt(b[0]);
+      return numA - numB;
+    })
+    .map(([id, name]) => {
+      return `${id}. ${name.split('-').join(' ')}`;
+    });
 
-      fs.renameSync(oldPath, newPath);
-    }
-  });
+  fs.writeFileSync('./README.md', questions.join('\n'));
 });
