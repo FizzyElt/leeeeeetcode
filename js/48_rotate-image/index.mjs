3024e78c629831dfb96475df3792d6f1f0c238a0
FizzyElt <fizzyelt.dev@outlook.com>
2024-10-11 20:30:46 +0800

    update 48 solution

--- js/48_rotate-image/index.mjs
+++ js/48_rotate-image/index.mjs
@@ -1,15 +1,28 @@
+/**
+ * - + \ = right 90 deg
+ * - + / = left 90 deg
+ * | + \ = left 90 deg
+ * | + / = right 90 deg
+ */
+
 function rotate(matrix) {
-  const len = matrix.length;
+	const len = matrix.length;
 
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len - i; j++) {
-      const temp = matrix[i][j];
-      matrix[i][j] = matrix[len - 1 - j][len - 1 - i];
-      matrix[len - 1 - j][len - 1 - i] = temp;
-    }
-  }
+	// reverse
+	for (let i = 0; i < len >> 1; i++) {
+		const temp = matrix[i];
+		matrix[i] = matrix[len - i - 1];
+		matrix[len - i - 1] = temp;
+	}
 
-  matrix.reverse()
+	// flip
+	for (let i = 0; i < len; i++) {
+		for (let j = 0; j < i; j++) {
+			const temp = matrix[i][j];
+			matrix[i][j] = matrix[j][i];
+			matrix[j][i] = temp;
+		}
+	}
 
-  return matrix;
+	return matrix;
 }
